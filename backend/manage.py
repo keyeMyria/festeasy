d205e1a56753bade539f99433018188eb39d0296
Jason Russell <jasrusable@gmail.com>
2015-04-24 20:47:21 +0200

    create manage.py

---
+++ backend/manage.py
@@ -0,0 +1,32 @@
+import sys, os
+sys.path.append(os.path.dirname(
+	os.path.dirname(
+		os.path.abspath(__file__))
+	)
+)
+
+from flask.ext.script import Manager, Command, Option
+from flask.ext.script import Shell, Server
+
+from backend import create_app
+
+
+manager = Manager(create_app)
+manager.add_option('-c', '--env', dest='env', default='dev', required=False)
+
+class RunServer(Server):
+    def handle(self, *args, **kwargs):
+        logger.info("Starting development server")
+        Server.handle(self, *args, **kwargs)
+
+manager.add_command('run-api', RunServer(use_debugger=True, use_reloader=True, host='0.0.0.0'))
+
+def _make_context():
+    context = dict(db=db, current_app=current_app)
+    return context
+
+manager.add_command('shell', Shell(make_context=_make_context, use_ipython=True))
+
+
+if __name__ == '__main__':
+	manager.run()
