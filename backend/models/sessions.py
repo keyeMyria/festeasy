ecd9bbe03235ac8423926bdc0fa8293435944673
Jason Russell <jasrusable@gmail.com>
2015-04-27 21:04:28 +0200

    formatting

--- backend/models/sessions.py
+++ backend/models/sessions.py
@@ -9,6 +9,7 @@ from backend.models import Entity
 
 class Session(db.Model, Entity):
     __tablename__ = 'session'
+    
     user_id = Column(Integer, ForeignKey('user.id'))
     user = relationship('User', back_populates='sessions', cascade='save-update, merge')
     expires_on = Column(DateTime, nullable=False)
--- backend/models/users.py
+++ backend/models/users.py
@@ -7,9 +7,11 @@ from backend.models import Entity, Dumpable
 
 class User(db.Model, Entity, Dumpable):
     __tablename__ = 'user'
+
     whitelist = [
         'email_address'
     ]
+    
     email_address = Column(String(200), unique=True, nullable=False)
     sessions = relationship('Session', back_populates="user", cascade='save-update, merge, delete, delete-orphan')
 
