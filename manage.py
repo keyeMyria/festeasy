8fbe8499eb5cbe64c79690ce0eef1c1f30600df1
Jason Russell <jasrusable@gmail.com>
2015-05-22 19:15:00 +0200

    add cost_rands

--- manage.py
+++ manage.py
@@ -56,13 +56,13 @@ class InitDB(Command):
                 first_name='TestName')
             ]
         products = [
-            Product(name='Castle Lite Beer', price_rands=2000),
-            Product(name='Lays Small Pack', price_rands=900),
-            Product(name='Coke Can', price_rands=900),
-            Product(name='Windhoek Beer', price_rands=2100),
-            Product(name='Text Chocolate', price_rands=900),
-            Product(name='KitKat Chocolate', price_rands=800),
-            Product(name='Jelly Beans', price_rands=700),
+            Product(name='Castle Lite Beer', cost_rands=100, price_rands=2000),
+            Product(name='Lays Small Pack', cost_rands=100, price_rands=900),
+            Product(name='Coke Can', cost_rands=100, price_rands=900),
+            Product(name='Windhoek Beer', cost_rands=100, price_rands=2100),
+            Product(name='Text Chocolate', cost_rands=100, price_rands=900),
+            Product(name='KitKat Chocolate', cost_rands=100, price_rands=800),
+            Product(name='Jelly Beans', cost_rands=100, price_rands=700),
             ]
         events = [
             Event(name='Rocking The Daisies'),
