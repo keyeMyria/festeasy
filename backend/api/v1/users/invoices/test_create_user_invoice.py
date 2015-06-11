7d6c7c022e60576e8d4a80ba4e1a293317b4a8dd
Jason Russell <jasrusable@gmail.com>
2015-06-11 15:14:22 +0200

    test 404

--- backend/api/v1/users/invoices/test_create_user_invoice.py
+++ backend/api/v1/users/invoices/test_create_user_invoice.py
@@ -32,3 +32,27 @@ class TestCreateUserInvoice(APITestCase):
         self.assertEqual(response.status_code, 201)
         self.assertEqual(len(response.json['invoices']), 1)
         self.assertEqual(response.json['invoices'][0]['id'], 1)
+
+    def test_create_user_invoice_404s(self):
+        user = self.create_user(create_valid_session=True)
+        product = self.create_product(name='asd', price_rands=10)
+        user.cart.products.append(product)
+        event = self.create_event(name='asd')
+        user.cart.event = event
+        order = self.create_order(create_from_cart=True, user=user)
+
+        db.session.add(user)
+        db.session.add(order)
+        db.session.commit()
+
+        invalid_order_id = order.id + 1
+
+        response = self.api_request(
+            'post',
+            url_for('v1.create_user_invoice', user_id=user.id), 
+            as_user=user,
+            data=dict(order_id=invalid_order_id),
+            with_session=user.sessions[0],
+        )
+        
+        self.assertEqual(response.status_code, 404)
