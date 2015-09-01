e2fc35383a29a41409ddabf136e43d0a49d41e8b
Jason Russell <jasrusable@gmail.com>
2015-09-01 21:37:54 +0200

    Move schema into module, use in users collection

--- backend/api/utils/__init__.py
+++ backend/api/utils/__init__.py
@@ -1,1 +1,2 @@
 from .get_or_404 import get_or_404
+from .entity_schema import EntitySchema
--- backend/api/v1/resources/users/singleton/__init__.py
+++ backend/api/v1/resources/users/singleton/__init__.py
@@ -1,44 +1,15 @@
-from flask_restful import Resource
-from flask_restful import reqparse
-from marshmallow import Schema, fields, pre_load, pre_dump
 from flask import request
+from flask_restful import Resource
 
 from backend import db
 from backend.models import User
 from backend.api.utils import get_or_404
-
-
-class EntitySchema(Schema):
-    id = fields.Integer(dump_only=True)
-    created_on = fields.DateTime(dump_only=True)
-    last_updated_on = fields.DateTime(dump_only=True)
-
-
-class UserSchema(EntitySchema):
-    email_address = fields.Email()
-    first_name = fields.String()
-    last_name = fields.String()
-    is_admin = fields.Boolean()
-    cart_id = fields.Integer()
-    class Meta:
-        strict = True
-
-    @pre_load
-    def pre_load(self, data):
-        pass
-
-    @pre_dump
-    def pre_dump(self, data):
-        del self.fields['first_name']
+from ..schema import UserSchema
 
 
 class UserSingleton(Resource):
     def __init__(self):
-        self.user_schema = UserSchema(
-            context=dict(
-                authenticated_user='dummy authenticated user',
-            )
-        )
+        self.user_schema = UserSchema()
 
     def get(self, user_id):
         user = get_or_404(User, User.id == user_id)
