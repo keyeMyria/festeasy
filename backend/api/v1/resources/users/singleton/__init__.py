e9f91fa13152e09fd1876778e8fba59cc0fdbd5d
Jason Russell <jasrusable@gmail.com>
2015-09-01 16:45:38 +0200

    Play with MM schemas

--- backend/api/v1/resources/users/singleton/__init__.py
+++ backend/api/v1/resources/users/singleton/__init__.py
@@ -1,42 +1,63 @@
-from flask_restful import Resource, fields, marshal_with
+from flask_restful import Resource
 from flask_restful import reqparse
+from marshmallow import Schema, fields, pre_load, pre_dump
+from flask import request
 
 from backend import db
 from backend.models import User
 from backend.api.utils import get_or_404
 
 
-user_fields = {
-    'id': fields.Integer,
-    'email_address': fields.String,
-    'cart_id': fields.Integer,
-}
+class EntitySchema(Schema):
+    id = fields.Integer(dump_only=True)
+    created_on = fields.DateTime(dump_only=True)
+    last_updated_on = fields.DateTime(dump_only=True)
 
 
-patch_parser = reqparse.RequestParser()
-patch_parser.add_argument('first_name', type=str)
-patch_parser.add_argument('last_name', type=str)
+class UserSchema(EntitySchema):
+    email_address = fields.Email()
+    first_name = fields.String()
+    last_name = fields.String()
+    is_admin = fields.Boolean()
+    cart_id = fields.Integer()
+    class Meta:
+        strict = True
+
+    @pre_load
+    def pre_load(self, data):
+        pass
+
+    @pre_dump
+    def pre_dump(self, data):
+        del self.fields['first_name']
 
 
 class UserSingleton(Resource):
-    @marshal_with(user_fields)
+    def __init__(self):
+        self.user_schema = UserSchema(
+            context=dict(
+                authenticated_user='dummy authenticated user',
+            )
+        )
+
     def get(self, user_id):
         user = get_or_404(User, User.id == user_id)
-        return user
+        data, errors = self.user_schema.dump(user)
+        return data
 
-    @marshal_with(user_fields)
     def delete(self, user_id):
         user = get_or_404(User, User.id == user_id)
         db.session.delete(user)
         db.session.commit()
-        return user
+        data, errors = self.user_schema.dump(user)
+        return data
 
-    @marshal_with(user_fields)
     def patch(self, user_id):
-        args = patch_parser.parse_args(strict=True)
         user = get_or_404(User, User.id == user_id)
-        for arg in args:
-            setattr(user, arg, args[arg])
+        data, errors = self.user_schema.load(request.get_json())
+        for arg in data:
+            setattr(user, arg, data[arg])
         db.session.add(user)
         db.session.commit()
-        return user
+        data, errors = self.user_schema.dump(user)
+        return data
