e62f4f45d831a33252b7042337988b8697d20e06
Jason Russell <jasrusable@gmail.com>
2015-10-31 14:07:52 +0200

    Make gulp work

--- www/gulpfile.js
+++ www/gulpfile.js
@@ -6,22 +6,50 @@ var debug = require('gulp-debug')
 var gulpFilter = require('gulp-filter')
 var concat = require('gulp-concat')
 var less = require('gulp-less')
+var coffee = require('gulp-coffee')
+var flatten = require('gulp-flatten')
+var order = require('gulp-order')
 
 
-gulp.task('clean', function () {
-	return del(['./dist/*'])
+gulp.task('clean', function (cb) {
+	return del(['./dist/*'], cb);
 });
 
-gulp.task('wiredep', function () {
-	gulp.src('./src/index.html')
-		.pipe(wiredep())
+gulp.task('html', function () {
+	return gulp.src('./src/**/*.partial.html')
+		.pipe(flatten())
+		.pipe(gulp.dest('./dist/partials'));
+});
+
+gulp.task('coffee', function () {
+	return gulp.src('./src/**/*.coffee')
+		.pipe(coffee({bare: true}))
+		.pipe(order([
+			"**/*.module.js"
+		]))
+		.pipe(concat('src.js'))
 		.pipe(gulp.dest('./dist'));
 });
 
-gulp.task('bower', function () {
+gulp.task('assets', function () {
+	return gulp.src('./src/app/assets/**')
+		.pipe(gulp.dest('./dist/assets'));
+});
+
+gulp.task('index', function () {
+	return gulp.src('./src/index.html')
+		.pipe(gulp.dest('./dist'));
+});
+
+gulp.task('bootstrap-fonts', function () {
+	return gulp.src('./bower_components/bootstrap/fonts/**')
+		.pipe(gulp.dest('./dist/fonts'));
+});
+
+gulp.task('bower', ['bootstrap-fonts'], function () {
 	var jsFilter = gulpFilter(['**/*.js'], {restore: true});
 	var lessFilter = gulpFilter(['**/*.less'], {restore: true});
-	gulp.src(mainBowerFiles(), {base: 'bower_components'})
+	return gulp.src(mainBowerFiles(), {base: 'bower_components'})
 		.pipe(jsFilter)
 		.pipe(concat('bower.js'))
 		.pipe(jsFilter.restore)
@@ -29,6 +57,9 @@ gulp.task('bower', function () {
 		.pipe(less())
 		.pipe(concat('bower.css'))
 		.pipe(lessFilter.restore)
-		.pipe(debug())
 		.pipe(gulp.dest('./dist'));
+});
+
+gulp.task('default', ['clean'], function () {
+	gulp.start('html', 'coffee', 'assets', 'bower', 'index');
 });
