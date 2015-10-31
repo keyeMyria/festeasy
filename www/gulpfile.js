20ba0dba4ec310a5685b3fc671c3eebd02073d0f
Jason Russell <jasrusable@gmail.com>
2015-10-31 19:29:07 +0200

    Minify js and css

--- www/gulpfile.js
+++ www/gulpfile.js
@@ -10,75 +10,91 @@ var flatten = require('gulp-flatten');
 var order = require('gulp-order');
 var livereload = require('gulp-livereload');
 var templateCache = require('gulp-angular-templatecache');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var minifyCss = require('gulp-minify-css');
 
 
 gulp.task('clean', function (cb) {
-	return del(['./dist/*'], cb);
+    return del(['./dist/*'], cb);
 });
 
 gulp.task('bootstrap-fonts', function () {
-	return gulp.src('./bower_components/bootstrap/fonts/**')
-		.pipe(gulp.dest('./dist/fonts'));
+    return gulp.src('./bower_components/bootstrap/fonts/**')
+        .pipe(gulp.dest('./dist/fonts'));
 });
 
 gulp.task('bower', ['bootstrap-fonts'], function () {
-	var jsFilter = gulpFilter(['**/*.js'], {restore: true});
-	var lessFilter = gulpFilter(['**/*.less'], {restore: true});
-	return gulp.src(mainBowerFiles(), {base: 'bower_components'})
-		.pipe(jsFilter)
-		.pipe(concat('bower.js'))
-		.pipe(jsFilter.restore)
-		.pipe(lessFilter)
-		.pipe(less())
-		.pipe(concat('bower.css'))
-		.pipe(lessFilter.restore)
-		.pipe(gulp.dest('./dist'));
+    var jsFilter = gulpFilter(['**/*.js'], {restore: true});
+    var lessFilter = gulpFilter(['**/*.less'], {restore: true});
+    var cssFilter = gulpFilter(['**/*.css'], {restore: true});
+    return gulp.src(mainBowerFiles(), {base: 'bower_components'})
+        .pipe(jsFilter)
+        .pipe(concat('bower.js'))
+        .pipe(gulp.dest('./dist'))
+        .pipe(uglify())
+        .pipe(rename({
+            suffix: ".min"
+        }))
+        .pipe(gulp.dest('./dist'))
+        .pipe(jsFilter.restore)
+        .pipe(lessFilter)
+        .pipe(less())
+        .pipe(lessFilter.restore)
+        .pipe(cssFilter)
+        .pipe(concat('bower.css'))
+        .pipe(gulp.dest('./dist'))
+        .pipe(minifyCss())
+        .pipe(rename({
+            suffix: ".min"
+        }))
+        .pipe(gulp.dest('./dist'));
 });
 
 gulp.task('partials', function () {
-	return gulp.src('./src/**/*.partial.html')
-		.pipe(flatten())
-		.pipe(templateCache({module: 'app'}))
-		.pipe(gulp.dest('./dist'));
+    return gulp.src('./src/**/*.partial.html')
+        .pipe(flatten())
+        .pipe(templateCache({module: 'app'}))
+        .pipe(gulp.dest('./dist'));
 });
 
 gulp.task('scripts', function () {
-	return gulp.src('./src/**/*.coffee')
-		.pipe(coffee({bare: true}))
-		.pipe(order([
-			"**/*.module.js"
-		]))
-		.pipe(concat('src.js'))
-		.pipe(gulp.dest('./dist'));
+    return gulp.src('./src/**/*.coffee')
+        .pipe(coffee({bare: true}))
+        .pipe(order([
+            "**/*.module.js"
+        ]))
+        .pipe(concat('src.js'))
+        .pipe(gulp.dest('./dist'));
 });
 
 gulp.task('styles', function () {
-	return gulp.src('./src/**/*.css')
-		.pipe(concat('styles.css'))
-		.pipe(gulp.dest('./dist'));
+    return gulp.src('./src/**/*.css')
+        .pipe(concat('styles.css'))
+        .pipe(gulp.dest('./dist'));
 })
 
 gulp.task('assets', function () {
-	return gulp.src('./src/app/assets/**')
-		.pipe(gulp.dest('./dist/assets'));
+    return gulp.src('./src/app/assets/**')
+        .pipe(gulp.dest('./dist/assets'));
 });
 
 gulp.task('index', function () {
-	return gulp.src('./src/index.html')
-		.pipe(gulp.dest('./dist'));
+    return gulp.src('./src/index.html')
+        .pipe(gulp.dest('./dist'));
 });
 
 gulp.task('watch', function () {
-	gulp.watch('./bower_components/**', ['bower']);
-	gulp.watch('./src/**/*.partial.html', ['partials']);
-	gulp.watch('./src/**/*.coffee', ['scripts']);
-	gulp.watch('./src/**/*.css', ['styles']);
-	gulp.watch('./src/app/assets/**', ['assets']);
-	gulp.watch('./src/index.html', ['index']);
-	livereload.listen();
-	gulp.watch('./dist/**').on('change', livereload.changed);
+    gulp.watch('./bower_components/**', ['bower']);
+    gulp.watch('./src/**/*.partial.html', ['partials']);
+    gulp.watch('./src/**/*.coffee', ['scripts']);
+    gulp.watch('./src/**/*.css', ['styles']);
+    gulp.watch('./src/app/assets/**', ['assets']);
+    gulp.watch('./src/index.html', ['index']);
+    livereload.listen();
+    gulp.watch('./dist/**').on('change', livereload.changed);
 })
 
 gulp.task('default', ['clean'], function () {
-	gulp.start('partials', 'scripts', 'assets', 'bower', 'index', 'styles');
+    gulp.start('partials', 'scripts', 'assets', 'bower', 'index', 'styles');
 });
