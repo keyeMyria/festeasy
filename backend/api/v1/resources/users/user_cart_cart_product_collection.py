facaed04ec181d2c94eaabc139559166df116e87
Jason Russell <jasrusable@gmail.com>
2015-11-18 11:06:06 +0200

    Use marshal of fail

--- backend/api/v1/resources/users/user_cart_cart_product_collection.py
+++ backend/api/v1/resources/users/user_cart_cart_product_collection.py
@@ -1,17 +1,18 @@
 from flask_restful import Resource
 
 from backend.api.v1.schemas import CartProductSchema
+from backend.api.utils import marshal_or_fail
 from backend.models import User, CartProduct, Cart
 
 
 class UserCartCartProductCollection(Resource):
-    def __init__(self):
-        self.cart_product_schema = CartProductSchema()
-
     def get(self, user_id):
         # TODO: Play with this query
         cart_products = (CartProduct.query.join(Cart).join(User)
                         .filter(User.id == user_id)
                         .all())
-        data, errors = self.cart_product_schema.dump(cart_products, many=True)
-        return data
+        return marshal_or_fail('dump', cart_products,
+            CartProductSchema(), many=True)
+
+    def post(self, user_id):
+        pass
