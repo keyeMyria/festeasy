f152299db75d1efd00fb07ea02d93edf53bd2e85
Jason Russell <jasrusable@gmail.com>
2015-12-05 18:07:07 +0200

    Improve filtering

--- backend/api/v1/resources/products/product_collection.py
+++ backend/api/v1/resources/products/product_collection.py
@@ -1,7 +1,7 @@
 from flask import request
 from flask_restful import Resource
 
-from backend.models import Product, Category
+from backend.models import Product, Category, ProductCategory
 from backend.api.utils import marshal_or_fail
 from backend.api.v1.schemas import ProductSchema
 
@@ -9,12 +9,10 @@ from backend.api.v1.schemas import ProductSchema
 class ProductCollection(Resource):
     def get(self):
         category = request.args.get('category')
+        q = Product.query
         if category:
-            c = Category.query.filter(Category.name == category).first()
-            if c:
-                products = c.products
-            else:
-                products = []
-        else:
-            products = Product.query.all()
+            q = q.join(ProductCategory)
+            q = q.join(Category)
+            q = q.filter(Category.name == category)
+        products = q.all()
         return marshal_or_fail('dump', products, ProductSchema(), many=True)
