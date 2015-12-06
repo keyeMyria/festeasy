fb2da5a8dabab9f80e20190dd0e6fce6a47d4c58
Jason Russell <jasrusable@gmail.com>
2015-12-06 18:01:45 +0200

    Pass test

--- backend/api/v1/schemas/user_schema.py
+++ backend/api/v1/schemas/user_schema.py
@@ -6,10 +6,10 @@ from backend.api.utils import EntitySchema
 class UserSchema(EntitySchema):
     first_name = fields.String()
     last_name = fields.String()
-    email_address = fields.Email(dump_only=True)
+    email_address = fields.Email()
     is_admin = fields.Boolean(dump_only=True)
     cart_id = fields.Integer(dump_only=True)
-    password = fields.String(dump_only=True)
+    password = fields.String()
 
     class Meta:
         strict = True
