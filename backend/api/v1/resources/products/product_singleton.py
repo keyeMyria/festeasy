bd12947dc2b8e0246b97e289d388002765678674
Jason Russell <jasrusable@gmail.com>
2015-12-11 18:27:10 +0200

    Implement Product patching

--- backend/api/v1/resources/products/product_singleton.py
+++ backend/api/v1/resources/products/product_singleton.py
@@ -1,5 +1,7 @@
+from flask import request
 from flask_restful import Resource
 
+from backend import db
 from backend.models import Product
 from backend.api.utils import get_or_404, marshal_or_fail
 from backend.api.v1.schemas import ProductSchema
@@ -8,4 +10,14 @@ from backend.api.v1.schemas import ProductSchema
 class ProductSingleton(Resource):
     def get(self, product_id):
         product = get_or_404(Product, Product.id == product_id)
+        return marshal_or_fail('dump', product, ProductSchema())
+
+    def patch(self, product_id):
+        product = get_or_404(Product, Product.id == product_id)
+        load_data = marshal_or_fail('load', request.get_json(), ProductSchema())
+        print(load_data)
+        for key, val in load_data.items():
+            setattr(product, key, val)
+        db.session.add(product)
+        db.session.commit()
         return marshal_or_fail('dump', product, ProductSchema())
--- backend/api/v1/resources/products/test_product_singleton.py
+++ backend/api/v1/resources/products/test_product_singleton.py
@@ -2,6 +2,7 @@ from flask import url_for
 
 from backend import db
 from backend.testing import APITestCase
+from backend.models import Product
 
 
 endpoint = 'v1.productsingleton'
@@ -18,3 +19,22 @@ class TestProductSingleton(APITestCase):
         )
         self.assertEqual(repsonse.status_code, 200)
         self.assertEqual(repsonse.json['id'], product.id)
+
+    def test_patch(self):
+        product = self.create_product(create_valid_product=True)
+        product_name = 'name_a'
+        product.name = product_name
+        new_product_name = 'name_b'
+        data = {
+            'name': new_product_name,
+        }
+        db.session.add(product)
+        db.session.commit()
+        repsonse = self.api_request(
+            'patch',
+            url_for(endpoint, product_id=product.id),
+            data=data,
+        )
+        self.assertEqual(repsonse.status_code, 200)
+        fetched_product = Product.query.one()
+        self.assertEqual(fetched_product.name, new_product_name)
