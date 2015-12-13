24d84666cd6e9af6f659fa67b458f03e39a44ea7
Jason Russell <jasrusable@gmail.com>
2015-12-13 16:54:59 +0200

    Add filter

--- backend/api/v1/resources/order_products/order_product_collection.py
+++ backend/api/v1/resources/order_products/order_product_collection.py
@@ -1,13 +1,24 @@
+from flask import request
 from flask_restful import Resource
 
-from backend.models import OrderProduct
+from backend.models import OrderProduct, Festival, Order
 from backend.api.utils import marshal_or_fail
 from backend.api.v1.schemas import OrderProductSchema
+
+
+def filter_festival(q):
+    festival_id = request.args.get('festival-id')
+    if festival_id:
+        q = q.join(Order)
+        q = q.join(Festival)
+        q = q.filter(Festival.id == festival_id)
+    return q
 
 
 class OrderProductCollection(Resource):
     def get(self):
         q = OrderProduct.query
+        q = filter_festival(q)
         order_products = q.all()
         return marshal_or_fail(
             'dump',
