6966884fd8f06fff9be24fa7fbdc80a0d5e58f0b
Jason Russell <jasrusable@gmail.com>
2015-12-29 17:24:24 +0200

    Use plural

--- backend/api/v1/resources/forgot_password_tokens/__init__.py
+++ backend/api/v1/resources/forgot_password_tokens/__init__.py
@@ -2,4 +2,4 @@ from backend.api.v1 import v1_api
 from .forgot_password_token_collection import ForgotPasswordTokenCollection
 
 v1_api.add_resource(ForgotPasswordTokenCollection,
-                    '/forgot-password-token')
+                    '/forgot-password-tokens')
