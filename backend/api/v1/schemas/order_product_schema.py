bc436bcad6ab9f5522374eb9805c607207c7691c
Jason Russell <jasrusable@gmail.com>
2016-02-05 21:33:01 +0200

    Add fields to schemas

--- backend/api/v1/schemas/order_product_schema.py
+++ backend/api/v1/schemas/order_product_schema.py
@@ -7,5 +7,6 @@ from .product_schema import ProductSchema
 
 class OrderProductSchema(EntitySchema):
     unit_price_rands = fields.Float()
+    quantity = fields.Integer()
     order = fields.Nested(OrderSchema)
     product = fields.Nested(ProductSchema)
