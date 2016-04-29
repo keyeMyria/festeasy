90b4afb1260433365d47360243e2d95f22ff3001
Jason Russell <jasrusable@gmail.com>
2016-04-29 21:01:18 +0200

    Add react router

--- new_www/src/index.jsx
+++ new_www/src/index.jsx
@@ -1,8 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, Link, browserHistory, IndexRoute } from 'react-router'
 
+
+const App = React.createClass({
+  render: function () {
+    return (
+      <div>
+        {this.props.children}
+      </div>
+    )
+  }
+})
+
+
+const Landing = React.createClass({
+  render: function () {
+    return (
+      <div>
+        <h1>Landing here</h1>
+      </div>
+    )
+  }
+})
+
+
+const routes =
+  <Router history={browserHistory}>
+    <Route path="/" component={App}>
+      <IndexRoute component={Landing}/>
+    </Route>
+  </Router>
 
 ReactDOM.render(
-  <h1>Hello</h1>,
+  routes,
   document.getElementById('reactContent')
 )
