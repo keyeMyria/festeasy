fee320677a9968b6a8b1cbc98aaaa87f922476f2
Jason Russell <jasrusable@gmail.com>
2016-04-29 23:13:07 +0200

    Move landing into seperate module

--- new_www/src/index.jsx
+++ new_www/src/index.jsx
@@ -5,27 +5,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, Link, browserHistory, IndexRoute } from 'react-router'
 
+import Landing from './landing.jsx'
+
 
 const App = React.createClass({
   render: function () {
     return (
       <div>
         {this.props.children}
-      </div>
-    )
-  }
-})
-
-
-const Landing = React.createClass({
-  render: function () {
-    return (
-      <div>
-        <h1 className="ui header">Landing here</h1>
-        <button className="ui button">
-          Follow
-        </button>
-        <Link to="/about">About</Link>
       </div>
     )
   }
@@ -43,13 +30,66 @@ const About = React.createClass({
 })
 
 
+const Admin = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <h1>Admin</h1>
+      </div>
+    )
+  }
+})
+
+
+const Store = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <h1>Store</h1>
+        <Menu />
+        {this.props.children}
+      </div>
+    )
+  }
+})
+
+
+const MenuItem = React.createClass({
+  render: function() {
+    return (
+        <Link className="item" to={this.props.to}>{this.props.text}</Link>
+    )
+  }
+})
+
+
+const Menu = React.createClass({
+  render: function() {
+    return (
+      <div className="ui fixed menu">
+        <div className="ui container">
+          <div className="header item">
+            <Link to="/">Home</Link>
+          </div>
+          <MenuItem to="/about" text="About" />
+        </div>
+      </div>
+    )
+  }
+})
+
+
 const routes =
   <Router history={browserHistory}>
     <Route path="/" component={App}>
-      <IndexRoute component={Landing}/>
-      <Route path="/about" component={About}/>
+      <Route path="" component={Store}>
+        <IndexRoute component={Landing}/>
+        <Route path="about" component={About}/>
+      </Route>
+      <Route path="admin" component={Admin}/>
     </Route>
   </Router>
+
 
 ReactDOM.render(
   routes,
