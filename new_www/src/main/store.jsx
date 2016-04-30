7cafcee35febb3ea9f3fc6e8125574c135fa2f5b
Jason Russell <jasrusable@gmail.com>
2016-04-30 13:03:51 +0200

    Add products

--- new_www/src/main/store.jsx
+++ new_www/src/main/store.jsx
@@ -1,11 +1,32 @@
 import React from 'react';
 
 
-const Store = React.createClass({
+const Product = React.createClass({
   render: function() {
     return (
       <div>
+        <h3>{this.props.product.name}</h3>
+        <p>{this.props.product.description}</p>
+        <p>Price: {this.props.product.price_rands}</p>
+      </div>
+    )
+  }
+})
+
+
+const Store = React.createClass({
+  render: function() {
+    const productNodes = this.props.products.map(function(product) {
+      return (
+        <div key={product.id}>
+          <Product product={product} />
+        </div>
+      )
+    })
+    return (
+      <div>
         <h1>Store</h1>
+        {productNodes}
       </div>
     )
   }
@@ -13,10 +34,31 @@ const Store = React.createClass({
 
 
 const StoreContainer = React.createClass({
+  getInitialState: function() {
+    return {
+      products: []
+    };
+  },
+
+
+  componentDidMount: function() {
+    this.serverRequest = $.get('http://localhost:5000/api/v1/products', function(result) {
+      this.setState({
+        products: result
+      })
+    }.bind(this))
+  },
+
+
+  componentWillUnmount: function() {
+    this.serverRequest.abort();
+  },
+
+
   render: function() {
     return (
       <div>
-        <Store />
+        <Store products={this.state.products}/>
       </div>
     )
   }
