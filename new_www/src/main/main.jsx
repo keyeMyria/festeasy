a6e05a8c904882bff2a53093c1d5f42644329ec9
Jason Russell <jasrusable@gmail.com>
2016-05-01 12:15:13 +0200

    Wrap children in container

--- new_www/src/main/main.jsx
+++ new_www/src/main/main.jsx
@@ -5,12 +5,12 @@ import Navigation from './navigation.jsx'
 const Main = React.createClass({
   render: function() {
     return (
-      <div>
-        <h1>Main</h1>
-        <h1>Main</h1>
+      <div style={{paddingTop: 55}}>
         <h1>Main</h1>
         <Navigation />
-        {this.props.children}
+        <div className="ui container">
+          {this.props.children}
+        </div>
       </div>
     )
   }
