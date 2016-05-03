d07f0cb650590fe0c4a44ec0a4544756107f9b47
Jason Russell <jasrusable@gmail.com>
2016-05-03 20:49:03 +0200

    Set up lodash

--- new_www/webpack.config.js
+++ new_www/webpack.config.js
@@ -11,6 +11,7 @@ module.exports = {
     },
     plugins: [
         new webpack.ProvidePlugin({
+            _: "lodash",
             $: "jquery",
             jQuery: "jquery",
             "window.jQuery": "jquery"
