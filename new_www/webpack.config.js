ea916d7cb3470bebc43092b7d17d00c1b305ee84
Jason Russell <jasrusable@gmail.com>
2016-05-28 16:57:47 +0200

    Clean up webpack file

--- new_www/webpack.config.js
+++ new_www/webpack.config.js
@@ -1,42 +1,45 @@
-var webpack = require('webpack');
+const webpack = require('webpack');
 
 
 module.exports = {
-    devtool: 'source-map',
-    entry:  './src/index.jsx',
-    output: {
-        path:       'build',
-        filename:   'bundle.js',
-        publicPath: 'build/'
-    },
-    plugins: [
-        new webpack.ProvidePlugin({
-            _: "lodash",
-            $: "jquery",
-            jQuery: "jquery",
-            "window.jQuery": "jquery"
-        })
+  devtool: 'source-map',
+  entry: './src/index.jsx',
+  output: {
+    path: 'build',
+    filename: 'bundle.js',
+    publicPath: 'build/',
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      _: 'lodash',
+      $: 'jquery',
+      jQuery: 'jquery',
+      'window.jQuery': 'jquery',
+    }),
+  ],
+  devServer: {
+    historyApiFallback: true,
+  },
+  module: {
+    preLoaders: [
+      {
+        test: /\.jsx$/,
+        loader: 'eslint-loader',
+      },
     ],
-    devServer: {
-      historyApiFallback: true
-    },
-    module: {
-       preLoaders: [
-            {test: /\.jsx$/, loader: "eslint-loader"}
-        ],
-        loaders: [
-            {
-                test: /\.jsx$/,
-                loaders: ['react-hot', 'babel-loader?presets[]=es2015&presets[]=react']
-            },
-            {
-                test: /\.css$/,
-                loader: 'style-loader!css-loader'
-            },
-            {
-                test: /\.(eot|woff|woff2|ttf|svg|png|jpg)$/,
-                loader: 'url-loader?limit=30000&name=[name]-[hash].[ext]'
-            }
-        ]
-    }
+    loaders: [
+      {
+        test: /\.jsx$/,
+        loaders: ['react-hot', 'babel-loader?presets[]=es2015&presets[]=react'],
+      },
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader',
+      },
+      {
+        test: /\.(eot|woff|woff2|ttf|svg|png|jpg)$/,
+        loader: 'url-loader?limit=30000&name=[name]-[hash].[ext]',
+      },
+    ],
+  },
 };
