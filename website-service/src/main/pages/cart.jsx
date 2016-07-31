cf3b99d47204c39893549567ce19c5b72991a575
Jason Russell <jasrusable@gmail.com>
2016-07-31 15:10:47 +0200

    Fix up some cart things

--- website-service/src/main/pages/cart.jsx
+++ website-service/src/main/pages/cart.jsx
@@ -1,8 +1,70 @@
 import React, { PropTypes } from 'react'
 import { Link } from 'react-router'
-import { Button, Input, Option } from 'semantic-react'
+import { Input, Option } from 'semantic-react'
+import update from 'react-addons-update'
+import PriceFormatter from 'utils/priceFormatter.jsx'
 import MySelect from 'utils/mySelect.jsx'
 import Page from 'common/page.jsx'
+
+
+class CartRow extends React.Component {
+  static propTypes = {
+    cartProduct: PropTypes.object.isRequired,
+    updateQuantity: PropTypes.func.isRequired,
+    removeCartProduct: PropTypes.func.isRequired,
+  }
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      cartProduct: props.cartProduct,
+    }
+    this.onBlur = this.onBlur.bind(this)
+  }
+
+  componentWillReceiveProps(nextProps) {
+    this.setState({
+      cartProduct: nextProps.cartProduct,
+    })
+  }
+
+  onBlur() {
+    this.props.updateQuantity(this.state.cartProduct)
+  }
+
+  updateQuantity(e) {
+    const { cartProduct } = this.state
+    this.setState({
+      cartProduct: update(cartProduct, { $merge: { quantity: e.target.value } }),
+    })
+  }
+
+  render() {
+    const { cartProduct } = this.state
+    return (
+      <tr>
+        <td>{cartProduct.product.name}</td>
+        <td>
+          <Input
+            type="number"
+            onChange={(e) => this.updateQuantity(e)}
+            onBlur={this.onBlur}
+            value={cartProduct.quantity}
+          />
+        </td>
+        <td><PriceFormatter rands={cartProduct.sub_total_rands} /></td>
+        <td>
+          <button
+            className="ui button"
+            onClick={() => { this.props.removeCartProduct(cartProduct) }}
+          >
+            Remove
+          </button>
+        </td>
+      </tr>
+    )
+  }
+}
 
 
 class Cart extends React.Component {
@@ -16,7 +78,13 @@ class Cart extends React.Component {
   }
 
   getMain() {
-    const { cart, festivals, updateQuantity, onCheckout } = this.props
+    const {
+      cart,
+      festivals,
+      updateQuantity,
+      onCheckout,
+      removeCartProduct,
+    } = this.props
     const options = festivals.map((festival) => (
       <Option key={festival.id} value={festival.id}>{festival.name}</Option>
     ))
@@ -25,7 +93,8 @@ class Cart extends React.Component {
         <label>Select festival:</label>
         <MySelect
           fluid
-          selected={[cart.festival_id]}
+          placeholder="Select festival"
+          selected={cart.festival_id ? [cart.festival_id] : []}
           updateSelected={(selected) => this.props.selectFestival(selected[0])}
           options={options}
         />
@@ -40,30 +109,19 @@ class Cart extends React.Component {
           </thead>
           <tbody>
             {cart.cart_products.map((cartProduct) => (
-              <tr key={cartProduct.id}>
-                <td>{cartProduct.product.name}</td>
-                <td>
-                  <Input
-                    type="number"
-                    onChange={(e) => updateQuantity(e, cartProduct)}
-                    value={cartProduct.quantity}
-                  />
-                </td>
-                <td>{cartProduct.sub_total_rands}</td>
-                <td>
-                  <button
-                    className="ui button"
-                    onClick={() => { this.props.removeCartProduct(cartProduct) }}
-                  >
-                    Remove
-                  </button>
-                </td>
-              </tr>
+              <CartRow
+                updateQuantity={updateQuantity}
+                removeCartProduct={removeCartProduct}
+                key={cartProduct.id}
+                cartProduct={cartProduct}
+              />
             ))}
           </tbody>
         </table>
-        <div>Total: R{cart.total_rands}</div>
-        <Button onClick={onCheckout}>Secure Checkout</Button>
+        <div>
+          <div>Total: <PriceFormatter rands={cart.total_rands} /></div>
+          <div className="ui positive button" onClick={onCheckout}>Secure Checkout</div>
+        </div>
       </div>
     )
   }
@@ -71,7 +129,6 @@ class Cart extends React.Component {
   render() {
     const { cart } = this.props
     let result
-    console.log(cart)
     if (cart.cart_products.length === 0) {
       result = (
         <h2 className="ui center aligned header">
@@ -104,42 +161,59 @@ export default class CartContainer extends React.Component {
       error: null,
     }
     this.onCheckout = this.onCheckout.bind(this)
-    this.getCart = this.getCart.bind(this)
+    this.fetchCart = this.fetchCart.bind(this)
     this.removeCartProduct = this.removeCartProduct.bind(this)
     this.selectFestival = this.selectFestival.bind(this)
     this.updateQuantity = this.updateQuantity.bind(this)
   }
 
   componentDidMount() {
-    this.getCart()
+    this.fetchCart()
+    this.fetchFestivals()
   }
 
   onCheckout() {
     this.context.router.push('/checkout/review')
   }
 
-  getCart() {
-    const { store, authDetails } = this.context
-    store.ejectAll('cartProduct')
-    const userFetch = store.find('user', authDetails.userId)
-    const festivalsFetch = store.findAll('festival')
-    userFetch.then((user) => {
-      const cartFetch = store.find(
-        'cart',
-        user.id,
-        { bypassCache: true }
-      )
-      Promise.all([cartFetch, festivalsFetch])
-        .then((values) => {
+  fetchFestivals() {
+    const { store } = this.context
+    return new Promise((resolve, reject) => {
+      store.findAll('festival')
+        .then((festivals) => {
           this.setState({
-            cart: values[0],
-            festivals: values[1],
-          })
+            festivals,
+          }, resolve(festivals))
         })
-        .catch(() => {
+        .catch((error) => {
           this.setState({
-            error: 'Something went wrong.',
-          })
+            error: 'Something went wront',
+          }, reject(error))
+        })
+    })
+  }
+
+  fetchCart() {
+    return new Promise((resolve, reject) => {
+      const { store, authDetails } = this.context
+      store.find('user', authDetails.userId)
+        .then((user) => {
+          store.find('cart', user.id, { bypassCache: true })
+            .then((cart) => {
+              this.setState({
+                cart,
+              }, resolve(cart))
+            })
+            .catch((error) => {
+              this.setState({
+                error: 'Something went wrong',
+              }, reject(error))
+            })
+        })
+        .catch((error) => {
+          this.setState({
+            error: 'Something went wrong',
+          }, reject(error))
         })
     })
   }
@@ -147,28 +221,44 @@ export default class CartContainer extends React.Component {
   removeCartProduct(cp) {
     this.context.store.destroy('cartProduct', cp.id)
       .then(() => {
-        this.getCart()
+        this.fetchCart()
       })
   }
 
-  updateQuantity(e, updatedCp) {
-    this.state.cart.cart_products.forEach((cp) => {
-      if (cp.id === updatedCp.id) {
-        console.log('Update the quantity.')
-      }
-    })
+  updateQuantity(cp) {
+    const { store } = this.context
+    store.update(
+      'cartProduct',
+      cp.id,
+      { quantity: cp.quantity },
+      { method: 'patch' },
+    )
+      .then(() => {
+        this.fetchCart()
+      })
   }
 
   selectFestival(festivalId) {
-    this.context.store.update(
-      'cart',
-      this.state.cart.id,
-      { festival_id: festivalId },
-      { method: 'PATCH' }
-    )
-      .then(() => {
-        this.getCart()
-      })
+    return new Promise((resolve, reject) => {
+      this.context.store.update(
+        'cart',
+        this.state.cart.id,
+        { festival_id: festivalId },
+        { method: 'PATCH' }
+      )
+        .then(() => {
+          this.fetchCart()
+            .then(() => {
+              resolve()
+            })
+            .catch(() => {
+              reject()
+            })
+        })
+        .catch(() => {
+          reject()
+        })
+    })
   }
 
   render() {
@@ -189,7 +279,8 @@ export default class CartContainer extends React.Component {
                 selectFestival={this.selectFestival}
                 updateQuantity={this.updateQuantity}
                 onCheckout={this.onCheckout}
-              /> : ''
+              />
+            : ''
           }
         />
       </div>
--- website-service/src/models.js
+++ website-service/src/models.js
@@ -2,26 +2,10 @@ module.exports = [
   {
     name: 'cart',
     endpoint: 'carts',
-    relations: {
-      hasMany: {
-        cartProduct: {
-          localField: 'cart_products',
-          foreignKey: 'cart_id',
-        },
-      },
-    },
   },
   {
     name: 'cartProduct',
     endpoint: 'cart-products',
-    relations: {
-      belongsTo: {
-        cart: {
-          localField: 'cart',
-          localKey: 'cart_id',
-        },
-      },
-    },
   },
   {
     name: 'product',
