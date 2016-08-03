456754070748e11c78614b84bd868669d6625cda
Jason Russell <jasrusable@gmail.com>
2016-08-03 13:30:08 +0200

    Refactor cart code

---
+++ website-service/src/main/myCart.jsx
@@ -0,0 +1,87 @@
+import React, { PropTypes } from 'react'
+import { Link } from 'react-router'
+import { Option } from 'semantic-react'
+import PriceFormatter from 'utils/priceFormatter.jsx'
+import MySelect from 'utils/mySelect.jsx'
+import CartRow from 'main/myCartRow.jsx'
+
+
+export default class Cart extends React.Component {
+  static propTypes = {
+    cart: PropTypes.object.isRequired,
+    cartProducts: PropTypes.array.isRequired,
+    festivals: PropTypes.array.isRequired,
+    removeCartProduct: PropTypes.func.isRequired,
+    selectFestival: PropTypes.func.isRequired,
+    updateQuantity: PropTypes.func.isRequired,
+    onCheckout: PropTypes.func.isRequired,
+  }
+
+  getMain() {
+    const {
+      cart,
+      cartProducts,
+      festivals,
+      updateQuantity,
+      onCheckout,
+      removeCartProduct,
+    } = this.props
+    const options = festivals.map((festival) => (
+      <Option key={festival.id} value={festival.id}>{festival.name}</Option>
+    ))
+    return (
+      <div className="ui segment">
+        <label htmlFor="MySelect">Select festival:</label>
+        <MySelect
+          fluid
+          placeholder="Select festival"
+          selected={cart.festival_id ? [cart.festival_id] : []}
+          updateSelected={(selected) => this.props.selectFestival(selected[0])}
+          options={options}
+        />
+        <table className="ui table">
+          <thead>
+            <tr>
+              <th>Product</th>
+              <th>Quantity</th>
+              <th>Sub Total</th>
+              <th />
+            </tr>
+          </thead>
+          <tbody>
+            {cartProducts.map((cartProduct) => (
+              <CartRow
+                updateQuantity={updateQuantity}
+                removeCartProduct={removeCartProduct}
+                key={cartProduct.id}
+                cartProduct={cartProduct}
+              />
+            ))}
+          </tbody>
+        </table>
+        <div>
+          <div>Total: <PriceFormatter rands={cart.total_rands} /></div>
+          <div className="ui positive button" onClick={onCheckout}>Secure Checkout</div>
+        </div>
+      </div>
+    )
+  }
+
+  render() {
+    const { cartProducts } = this.props
+    let result
+    if (cartProducts.length === 0) {
+      result = (
+        <h2 className="ui center aligned header">
+          Cart Empty
+          <div className="sub header">
+            <Link to="/store">Continue shopping</Link>
+          </div>
+        </h2>
+      )
+    } else {
+      result = this.getMain()
+    }
+    return result
+  }
+}
