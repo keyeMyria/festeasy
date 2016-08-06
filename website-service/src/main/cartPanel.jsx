6a83da6f0d627b1e87133b1fd480405ba45128d8
Craig Nielsen <craiglbn@gmail.com>
2016-08-06 13:29:44 +0200

    Add sideapanel wip

--- website-service/src/main/cartPanel.jsx
+++ website-service/src/main/cartPanel.jsx
@@ -1,5 +1,5 @@
 import React, { Component, PropTypes } from 'react';
-import { MenuItem, Tabs, TabMenu, Tab } from 'semantic-react'
+import { MenuItem, Tabs, TabMenu } from 'semantic-react'
 import CartItem from './cartItem.jsx'
 
 export default class CartPanel extends Component {
@@ -16,7 +16,7 @@ export default class CartPanel extends Component {
       <Tabs onTabChange={(val) => this.setState({ active: val })} activeTab={this.state.active}>
         <TabMenu vertical >
           <MenuItem menuValue={1}>
-            <CartItem />
+          hi
           </MenuItem>
           <MenuItem menuValue={2}>second</MenuItem>
           <MenuItem menuValue={3}>Third</MenuItem>
