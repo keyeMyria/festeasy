f1e140915091a7b294718ab2f6609d1a513d88bb
Jason Russell <jasrusable@gmail.com>
2016-08-06 12:52:52 +0200

    Add invoice product collection

--- backend-service/backend/api/v1/schemas/invoice_product_schema.py
+++ backend-service/backend/api/v1/schemas/invoice_product_schema.py
@@ -1,10 +1,12 @@
 from marshmallow import fields
 
-from . import EntitySchema
+from . import EntitySchema, ProductSchema
 
 
 class InvoiceProductSchema(EntitySchema):
     order_id = fields.Integer()
     unit_price_rands = fields.Float()
     quantity = fields.Integer()
+    sub_total_rands = fields.Float()
     product_id = fields.Integer()
+    product = fields.Nested(ProductSchema())
