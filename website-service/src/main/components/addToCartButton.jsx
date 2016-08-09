813b322780863427fac0a927af77ffa5f7ed325e
Jason Russell <jasrusable@gmail.com>
2016-08-09 19:00:54 +0200

    Add quantity option when adding things to carts

--- website-service/src/main/components/addToCartButton.jsx
+++ website-service/src/main/components/addToCartButton.jsx
@@ -1,4 +1,5 @@
 import React, { PropTypes } from 'react'
+import { Button, Input } from 'semantic-react'
 
 
 export default class AddToCartButton extends React.Component {
@@ -14,7 +15,11 @@ export default class AddToCartButton extends React.Component {
 
   constructor() {
     super()
+    this.state = {
+      quantity: 1,
+    }
     this.onClick = this.onClick.bind(this)
+    this.onChange = this.onChange.bind(this)
   }
 
   onClick() {
@@ -25,32 +30,53 @@ export default class AddToCartButton extends React.Component {
           store.create('cartProduct', {
             'cart_id': user.cart_id,
             'product_id': this.props.product.id,
+            quantity: this.state.quantity,
           })
             .then(() => {
               addNotification({
-                message: 'Successfully added product to cart.',
+                message: 'Successfully added product to cart',
                 level: 'success',
               })
             })
-            .catch(() => {
+            .catch((error) => {
+              let message = 'Something went wrong'
+              let level = 'error'
+              if (error.status === 409) {
+                message = 'Item already in cart'
+                level = 'warning'
+              }
               addNotification({
-                message: 'Something went wrong',
-                level: 'warning',
+                message,
+                level,
               })
             })
         })
     }
     if (!authDetails) {
       addNotification({
-        message: 'Please sign in to add products.',
+        message: 'Please sign in',
         level: 'warning',
       })
     }
   }
 
+  onChange(e) {
+    this.setState({ quantity: e.target.value })
+  }
+
   render() {
     return (
-      <button className="ui button" onClick={this.onClick}>Add to cart</button>
+      <div>
+        <Input
+          required
+          type="number"
+          min={1}
+          max={10}
+          value={this.state.quantity}
+          onChange={this.onChange}
+        />
+        <Button basic color="green" onClick={this.onClick}>Add to cart</Button>
+      </div>
     )
   }
 }
