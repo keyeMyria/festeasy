3138339d03820b54eedc9fad7c43df96fdd60d46
Craig Nielsen <craiglbn@gmail.com>
2016-08-14 15:40:05 +0200

    Add my own container for items

--- website-service/src/main/components/cartPanel/cartItem.jsx
+++ website-service/src/main/components/cartPanel/cartItem.jsx
@@ -1,6 +1,7 @@
 import React, { PropTypes } from 'react';
 import apiEndpoint from 'apiEndpoint.js'
-import { Image } from 'semantic-react'
+import { Image, Button } from 'semantic-react'
+import MyStatefulInput from 'utils/myStatefulInput.jsx'
 
 export default class CartItem extends React.Component {
   static propTypes = {
@@ -10,55 +11,43 @@ export default class CartItem extends React.Component {
   }
   render() {
     // , updateQuantity, removeCartProduct
-    const { cartProduct } = this.props
-    console.log('cartProduct: ', cartProduct)
+    const { cartProduct, updateQuantity, removeCartProduct } = this.props
     return (
-      <div>
-        <div className="ui items">
-          <div className="item" style={{padding: '5px', border: '1px solid',  borderColor: '#6AA0D5', borderRadius: 20}}>
-            <div style={{padding: '5px'}}>
-              <p>{parseInt(cartProduct.quantity, 10)}X</p>
-            </div>
-            <a className="header">{cartProduct.product.name}</a>
-            <div className="ui tiny image">
-              {cartProduct.product.thumbnail_image_id ?
-                <Image
-                  centered
-                  style={{ 'maxHeight': '270px', width: '70px', height: '70px' }}
-                  alt="product thumbnail"
-                  src={apiEndpoint.concat(`v1/images/${cartProduct.product.thumbnail_image_id}/image`)}
-                /> : 'No thumbnail image'
-              }
-            </div>
-            <div className="content">
-              <div className="ui grid" >
-                <div className="four wide column" >
-                  <div className="meta">
-                    <span>{cartProduct.product.description}</span>
-                  </div>
-                </div>
-                <div className="four wide column" >
-                  <div className="description">
-                  </div>
-                </div>
-                <div className="four wide column" >
-                  <div className="extra">
-                    Have a jolly good time
-                  </div>
-                </div>
-                <div className="four wide column" >
-                  Total:
-                  <div></div>
-                  <div className="ui blue header">
-                    {cartProduct.sub_total_rands}
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="ui hidden divider" />
+      <div className="ui grid">
+        <div className="three wide column">
+          {cartProduct.product.thumbnail_image_id ?
+            <Image
+              centered
+              style={{ 'maxHeight': '270px', width: '60px', height: '60px' }}
+              alt="product thumbnail"
+              src={apiEndpoint.concat(`v1/images/${cartProduct.product.thumbnail_image_id}/image`)}
+            /> : 'No thumbnail image'
+          }
         </div>
+        <div className="ten wide column">
+          <div className="row ui header">
+          {cartProduct.product.name}
+          </div>
+          <div className="row">
+          {cartProduct.product.description}
+          </div>
+        </div>
+        <div className="three wide column">
+          <MyStatefulInput
+            type="number"
+            min={1}
+            max={10}
+            initialValue={cartProduct.quantity}
+            onBlur={(e) => updateQuantity(cartProduct, e.target.value)}
+          />
+          <div>
+            R {cartProduct.sub_total_rands}
+          </div>
+          <a onClick={() => removeCartProduct(cartProduct)}>
+            Remove
+          </a>
+        </div>
+
       </div>
     )
-  }
-}
+  } }
