c41cd07de84f47cc9c888699a6e3e9413b8a911b
Craig Nielsen <craiglbn@gmail.com>
2016-08-16 20:52:40 +0200

    Add something

--- website-service/src/main/components/cartPanel/cartItem.jsx
+++ website-service/src/main/components/cartPanel/cartItem.jsx
@@ -13,41 +13,40 @@ export default class CartItem extends React.Component {
     // , updateQuantity, removeCartProduct
     const { cartProduct, updateQuantity, removeCartProduct } = this.props
     return (
-      <div className="ui grid">
-        <div className="three wide column">
-          {cartProduct.product.thumbnail_image_id ?
-            <Image
-              centered
-              style={{ 'maxHeight': '270px', width: '60px', height: '60px' }}
-              alt="product thumbnail"
-              src={apiEndpoint.concat(`v1/images/${cartProduct.product.thumbnail_image_id}/image`)}
-            /> : 'No thumbnail image'
-          }
-        </div>
-        <div className="nine wide column">
-          <div className="row ui header">
-          {cartProduct.product.name}
+          <div className="ui grid" >
+            <div className="three wide column">
+              {cartProduct.product.thumbnail_image_id ?
+                <Image
+                  centered
+                  style={{ 'maxHeight': '270px', width: '60px', height: '60px' }}
+                  alt="product thumbnail"
+                  src={apiEndpoint.concat(`v1/images/${cartProduct.product.thumbnail_image_id}/image`)}
+                /> : 'No thumbnail image'
+              }
+            </div>
+            <div className="nine wide column">
+              <div className="row ui header">
+                {cartProduct.product.name}
+              </div>
+              <div className="row">
+                {cartProduct.product.description}
+              </div>
+            </div>
+            <div className="four wide column">
+              <MyStatefulInput
+                type="number"
+                min={1}
+                max={10}
+                initialValue={cartProduct.quantity}
+                onBlur={(e) => updateQuantity(cartProduct, e.target.value)}
+              />
+              <div>
+                R {cartProduct.sub_total_rands}
+              </div>
+              <a onClick={() => removeCartProduct(cartProduct)}>
+                Remove
+              </a>
+            </div>
           </div>
-          <div className="row">
-          {cartProduct.product.description}
-          </div>
-        </div>
-        <div className="four wide column">
-          <MyStatefulInput
-            type="number"
-            min={1}
-            max={10}
-            initialValue={cartProduct.quantity}
-            onBlur={(e) => updateQuantity(cartProduct, e.target.value)}
-          />
-          <div>
-            R {cartProduct.sub_total_rands}
-          </div>
-          <a onClick={() => removeCartProduct(cartProduct)}>
-            Remove
-          </a>
-        </div>
-
-      </div>
     )
   } }
