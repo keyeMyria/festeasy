2cc579a0d8157855d21fd2820c30ce016b5fbce2
Jason Russell <jasrusable@gmail.com>
2016-08-19 16:49:04 +0200

    Use image loader

--- website-service/webpack.loaders.js
+++ website-service/webpack.loaders.js
@@ -28,7 +28,14 @@ const loaders = [
     loader: ExtractTextPlugin.extract('style-loader', 'css-loader'),
   },
   {
-    test: /\.(eot|woff|woff2|ttf|svg|png|jpg)$/,
+    test: /\.(jpe?g|png|gif|svg)$/i,
+    loaders: [
+      'file?hash=sha512&digest=hex&name=[hash].[ext]',
+      'image-webpack?bypassOnDebug&optimizationLevel=8&interlaced=false',
+    ],
+  },
+  {
+    test: /\.(eot|woff|woff2|ttf)$/,
     loader: 'url-loader?limit=30000&name=[name]-[hash].[ext]',
   },
 ]
