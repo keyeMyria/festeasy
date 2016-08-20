a006c807a837553caa688204044ff5d12eccce00
Craig Nielsen <craiglbn@gmail.com>
2016-08-20 14:35:35 +0200

    Add query to collection

--- backend-service/backend/api/v1/resources/categories/category_collection.py
+++ backend-service/backend/api/v1/resources/categories/category_collection.py
@@ -1,13 +1,25 @@
+from flask import request
 from flask_restful import Resource
+from webargs import fields
+from webargs.flaskparser import parser
 
 from backend.models import Category
 from backend.api.v1.schemas import CategorySchema
 
 
 category_schema = CategorySchema()
+args = {
+    'name': fields.String(
+        load_from='name',
+    )
+}
 
 
 class CategoryCollection(Resource):
     def get(self):
-        categories = Category.query.all()
-        return category_schema.dump(categories, many=True).data
+        params = parser.parse(args, request)
+        name = params['name']
+        q = Category.query
+        if name is not None:
+            q = q.filter(Category.name == name)
+        return category_schema.dump(q.all(), many=True).data
