f67c7fd7d2b4cfa59b09e8fdb588e0d8da4e8532
Craig Nielsen <craiglbn@gmail.com>
2016-08-20 14:35:51 +0200

    Update group schema

--- backend-service/backend/api/v1/schemas/group_schema.py
+++ backend-service/backend/api/v1/schemas/group_schema.py
@@ -1,8 +1,10 @@
 from marshmallow import fields
 
 from . import EntitySchema
+from . import CategorySchema
 
 
 class GroupSchema(EntitySchema):
     name = fields.String()
     description = fields.String()
+    categories = fields.Nested(CategorySchema(), many=True)
