6b4e6f423359b553389cd619daae015480ad0072
Jason Russell <jasrusable@gmail.com>
2016-08-21 18:25:47 +0200

    Fix up order list

--- website-service/src/main/components/myOrderList.jsx
+++ website-service/src/main/components/myOrderList.jsx
@@ -1,6 +1,7 @@
 import React, { PropTypes } from 'react'
 import { Link } from 'react-router'
 import { Table, Tr, Td } from 'semantic-react'
+import PriceFormatter from 'utils/priceFormatter.jsx'
 
 
 export default class OrderList extends React.Component {
@@ -24,10 +25,14 @@ export default class OrderList extends React.Component {
         <tbody>
           {orders.map((o) => (
             <Tr key={o.id}>
-              <Td>{o.id}</Td>
+              <Td>Order {o.id}</Td>
               <Td>{o.festival.name}</Td>
-              <Td>{o.total_rands}</Td>
-              <Td>{o.current_invoice.amount_due_rands}</Td>
+              <Td>
+                <PriceFormatter rands={o.total_rands} />
+              </Td>
+              <Td>
+                <PriceFormatter rands={o.current_invoice.amount_due_rands} />
+              </Td>
               <Td>
                 <Link to={`/account/orders/${o.id}/invoice`}>See invoice</Link>
               </Td>
