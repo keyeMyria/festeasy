607aa88d1824e41e275319bf25dfd0fa3a292d46
Jason Russell <jasrusable@gmail.com>
2016-08-21 17:50:26 +0200

    Roughly improve payment feedback pages

--- website-service/src/main/pages/paymentRedirects/paymentConfirmation.jsx
+++ website-service/src/main/pages/paymentRedirects/paymentConfirmation.jsx
@@ -1,14 +1,35 @@
 import React from 'react'
-import { Header } from 'semantic-react'
+import { Header, Container, Grid, Column } from 'semantic-react'
 
 
 export default class PaymentConfirmation extends React.Component {
   render() {
     return (
-      <div>
-        <Header>Payment Confirmed</Header>
-        Payment confirmation here.
-      </div>
+      <Container>
+        <Header aligned="center">Payment Confirmed</Header>
+        <Grid centered>
+          <Column>
+            <p>
+              Yay! Thank you for using FestEasy!
+            </p>
+            <p>
+              We have emailed you your payment receit.
+            </p>
+            <p>
+              Please print this receit
+              and present it when collecting your order from FestEasy.
+            </p>
+            <p>
+              If you have any queries, please contact&nbsp;
+              <a
+                href="mailto:admin@festeasy.co.za"
+              >
+                admin@festeasy.co.za
+              </a>
+            </p>
+          </Column>
+        </Grid>
+      </Container>
     )
   }
 }
