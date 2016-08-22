01b0e7145a3e7231d6e95343e229547f597d69e7
Jason Russell <jasrusable@gmail.com>
2016-08-22 19:51:56 +0200

    Roughly set up how-it-works page

--- website-service/src/main/pages/about/about.jsx
+++ website-service/src/main/pages/about/about.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Header } from 'semantic-react'
 
 
 export default class About extends React.Component {
--- website-service/src/main/pages/howItWorks/howItWorks.jsx
+++ website-service/src/main/pages/howItWorks/howItWorks.jsx
@@ -3,9 +3,29 @@ import React from 'react'
 
 export default class HowItWorks extends React.Component {
   render() {
+    const steps = [
+      {
+        name: 'Select festival and products',
+      },
+      {
+        name: 'Checkout and Pay',
+      },
+      {
+        name: 'Recive order and enjoy the festival',
+      },
+    ]
     return (
-      <div>
-        How it works here
+      <div className="ui container">
+        <div className="ui fluid ordered steps">
+          {steps.map((s) => (
+            <div key={s.name} className="step">
+              <div className="content">
+                <div className="title">{s.name}</div>
+                <div className="description">{s.description}</div>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     )
   }
