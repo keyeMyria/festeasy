8c5228257da71c47bd4d6ca922b1ef5ce04569c2
Jason Russell <jasrusable@gmail.com>
2016-08-22 23:21:03 +0200

    Add user-id filter on order collection

--- backend-service/backend/api/v1/resources/orders/order_collection.py
+++ backend-service/backend/api/v1/resources/orders/order_collection.py
@@ -1,4 +1,7 @@
+from flask import request
 from flask_restful import Resource
+from webargs import fields
+from webargs.flaskparser import parser
 
 from backend.models import Order
 from backend.api.v1.schemas import OrderSchema
@@ -6,11 +9,22 @@ from backend.api.v1.authentication import requires_auth
 
 
 order_schema = OrderSchema()
+query_args = {
+    'user_id': fields.String(
+        load_from='user-id',
+        missing=None,
+    )
+}
 
 
 class OrderCollection(Resource):
     method_decorators = [requires_auth]
 
     def get(self, authenticated_user):
-        orders = Order.query.all()
-        return order_schema.dump(orders, many=True).data
+        params = parser.parse(query_args, request)
+        q = Order.query
+        # TODO: Test filter.
+        user_id = params['user_id']
+        if user_id:
+            q = q.filter(Order.user_id == user_id)
+        return order_schema.dump(q.all(), many=True).data
